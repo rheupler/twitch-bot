2d79690e03c832e6190d5bd30849eae887329ee0
ryan heupler <rheupler@gmail.com>
2016-09-27 19:08:38 -0700

    modified messages from bot

--- app.js
+++ app.js
@@ -19,9 +19,12 @@ var client = new tmi.client(options);
 client.connect();
 
 client.on('chat', function(channel, user, message, self) {
-  client.action('king_o_town', user['display-name'] + " don't say that")
-})
-
-client.on('connected', function(address, port) {
-  client.action("king_o_town", "King o Town is a noob");
+  // client.action('king_o_town', user['display-name'] + " don't say that");
+  if(message === '!sadsheeepbot') {
+    client.action('king_o_town', "Hello")
+  }
 });
+
+// client.on('connected', function(address, port) {
+//   client.action("king_o_town", "King o Town is a noob");
+// });
